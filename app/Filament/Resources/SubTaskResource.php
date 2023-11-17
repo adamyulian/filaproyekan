bda148c53d1288d4913a2d434e0a92d196d71d80
adamyulian <aadamyulian@Gmail.com>
2023-11-17 10:56:27 +0700

    Subtask done

--- app/Filament/Resources/SubTaskResource.php
+++ app/Filament/Resources/SubTaskResource.php
@@ -6,9 +6,13 @@ use App\Filament\Resources\SubTaskResource\Pages;
 use App\Filament\Resources\SubTaskResource\RelationManagers;
 use App\Models\SubTask;
 use Filament\Forms;
+use Filament\Forms\Components\Radio;
+use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
+use Filament\Tables\Columns\IconColumn;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
@@ -17,13 +21,15 @@ class SubTaskResource extends Resource
 {
     protected static ?string $model = SubTask::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-building-office';
 
     public static function form(Form $form): Form
     {
         return $form
             ->schema([
-                //
+                TextInput::make(name:'nama')->required(),
+                TextInput::make(name:'deskripsi')->required(),
+                Radio::make('is_published')->label('Is Published?')->boolean()
             ]);
     }
 
@@ -31,7 +37,12 @@ class SubTaskResource extends Resource
     {
         return $table
             ->columns([
-                //
+                TextColumn::make('nama')
+                ->sortable()
+                ->description(fn (SubTask $record): string => $record->deskripsi),
+                IconColumn::make('is_published')
+                ->label('Status Tayang')
+                ->boolean(),
             ])
             ->filters([
                 //
@@ -48,14 +59,14 @@ class SubTaskResource extends Resource
                 Tables\Actions\CreateAction::make(),
             ]);
     }
-    
+
     public static function getRelations(): array
     {
         return [
             //
         ];
     }
-    
+
     public static function getPages(): array
     {
         return [
@@ -63,5 +74,5 @@ class SubTaskResource extends Resource
             'create' => Pages\CreateSubTask::route('/create'),
             'edit' => Pages\EditSubTask::route('/{record}/edit'),
         ];
-    }    
+    }
 }
--- app/Policies/SubTaskPolicy.php
+++ app/Policies/SubTaskPolicy.php
@@ -8,59 +8,59 @@ use App\Models\User;
 
 class SubTaskPolicy
 {
-    /**
-     * Determine whether the user can view any models.
-     */
-    public function viewAny(User $user): bool
-    {
-        //
-    }
+    // /**
+    //  * Determine whether the user can view any models.
+    //  */
+    // public function viewAny(User $user): bool
+    // {
+    //     //
+    // }
 
-    /**
-     * Determine whether the user can view the model.
-     */
-    public function view(User $user, SubTask $subTask): bool
-    {
-        //
-    }
+    // /**
+    //  * Determine whether the user can view the model.
+    //  */
+    // public function view(User $user, SubTask $subTask): bool
+    // {
+    //     //
+    // }
 
-    /**
-     * Determine whether the user can create models.
-     */
-    public function create(User $user): bool
-    {
-        //
-    }
+    // /**
+    //  * Determine whether the user can create models.
+    //  */
+    // public function create(User $user): bool
+    // {
+    //     //
+    // }
 
-    /**
-     * Determine whether the user can update the model.
-     */
-    public function update(User $user, SubTask $subTask): bool
-    {
-        //
-    }
+    // /**
+    //  * Determine whether the user can update the model.
+    //  */
+    // public function update(User $user, SubTask $subTask): bool
+    // {
+    //     //
+    // }
 
-    /**
-     * Determine whether the user can delete the model.
-     */
-    public function delete(User $user, SubTask $subTask): bool
-    {
-        //
-    }
+    // /**
+    //  * Determine whether the user can delete the model.
+    //  */
+    // public function delete(User $user, SubTask $subTask): bool
+    // {
+    //     //
+    // }
 
-    /**
-     * Determine whether the user can restore the model.
-     */
-    public function restore(User $user, SubTask $subTask): bool
-    {
-        //
-    }
+    // /**
+    //  * Determine whether the user can restore the model.
+    //  */
+    // public function restore(User $user, SubTask $subTask): bool
+    // {
+    //     //
+    // }
 
-    /**
-     * Determine whether the user can permanently delete the model.
-     */
-    public function forceDelete(User $user, SubTask $subTask): bool
-    {
-        //
-    }
+    // /**
+    //  * Determine whether the user can permanently delete the model.
+    //  */
+    // public function forceDelete(User $user, SubTask $subTask): bool
+    // {
+    //     //
+    // }
 }
