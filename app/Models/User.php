946d3dd6f81fb73597df60ba77145d8b4f09341f
aadamyulian <aadamyulian@gmail.com>
2024-03-10 06:20:24 +0700

    sasa

--- app/Models/User.php
+++ app/Models/User.php
@@ -15,7 +15,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable 
-implements MustVerifyEmail, FilamentUser
+implements MustVerifyEmail
+// ,
+//  FilamentUser
 {
 
     
